3e085d7ed9dc30aeb5b31b9c962212a1b3c2f7ad
mbilal501 <61463001+mbilal501@users.noreply.github.com>
2020-02-25 19:02:43 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,148 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import './App.css';
+
+
+const app = new Clarifai.App({
+ apiKey: 'ba997cff067d44a5bb51a3620de0a6b4'
+});
+
+const particlesOptions = {
+   particles :{
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+   }
+}
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
+      
+    } 
+  }
+  
+calculateFaceLocation = (data) => {
+
+     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+     const image = document.getElementById('inputImage');
+     const width = Number(image.width);
+     const height = Number(image.height);
+     console.log(width,height);
+     return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow:  clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+
+     }
+
+
+}
+
+displayFaceBox = (box) => {
+  console.log(box);
+  this.setState({box: box});
+}
+
+
+
+onInputChange = (event) => {
+  this.setState({input: event.target.value});
+}
+
+onButtonSubmit = () => {
+  
+     this.setState({imageUrl: this.state.input});
+
+      app.models.predict(
+       Clarifai.FACE_DETECT_MODEL,
+         this.state.input)
+         //"https://samples.clarifai.com/face-det.jpg"
+         .then(response => this.displayFaceBox(this.calculateFaceLocation(response))) 
+  .catch(err => console.log(err));
+         
+     /* .then(
+    function(response) {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      // do something with response
+    },
+    function(err) {
+      // there was an error
+    }
+
+    );*/
+  
+
+
+}
+    
+  
+  
+      // do something with response
+     // console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    
+onRouteChange = (route) => {
+  if (route === 'signout'){
+    this.setState({isSignedIn: false})
+  } else if ( route === 'home'){
+    this.setState({isSignedIn: true})
+  }
+  this.setState({route: route});
+}
+
+
+render(){
+     const {isSignedIn, imageUrl, route, box,} = this.state;
+   return (
+    <div className="App">
+    <Particles className='particles'
+    params={particlesOptions}
+     />
+     
+     <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+     { route === 'home' 
+ 
+    ? <div>
+      <Logo/> 
+      <Rank/>
+      <ImageLinkForm
+       onInputChange={this.onInputChange}
+       onButtonSubmit={this.onButtonSubmit}
+      />
+      <FaceRecognition box={box} imageUrl={imageUrl} />
+       </div>
+      : ( route === 'signin'
+
+     ? <Signin onRouteChange={this.onRouteChange}/>
+
+     : <Register onRouteChange={this.onRouteChange}/>
+
+     )
+
+ }
+    </div>
+   );
+ }
+
+}
+
+export default App;
